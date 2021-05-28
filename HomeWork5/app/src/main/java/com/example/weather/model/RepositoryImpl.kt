adb3355a72d5acc7b14f32bc1afecc5c50eb8a75
dimov-k <dimov.k.k@gmail.com>
2021-05-28 11:21:03 +0300

    HomeWork5

---
+++ HomeWork5/app/src/main/java/com/example/weather/model/RepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.weather.model
+
+class RepositoryImpl : Repository {
+
+    override fun getWeatherFromServer(): Weather {
+        return Weather()
+    }
+
+    override fun getWeatherFromLocalStorageRus(): List<Weather> {
+        return getRussianCities()
+    }
+
+    override fun getWeatherFromLocalStorageWorld(): List<Weather> {
+        return getWorldCities()
+    }
+}
+
