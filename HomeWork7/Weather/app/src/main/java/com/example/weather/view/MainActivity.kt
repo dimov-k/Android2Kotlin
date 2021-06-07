130c8e4a107f34ec458b27d4a6f46cbd5971b4a2
dimov-k <dimov.k.k@gmail.com>
2021-06-07 07:52:09 +0300

    HomeWork7

---
+++ HomeWork7/Weather/app/src/main/java/com/example/weather/view/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.weather.view
+
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weather.R
+import com.example.weather.databinding.MainActivityBinding
+import com.example.weather.view.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+        private val receiver = BroadcastReceiver()
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.main_activity)
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitAllowingStateLoss()
+            }
+            registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+        }
+        override fun onDestroy() {
+            unregisterReceiver(receiver)
+            super.onDestroy()
+        }
+    }
